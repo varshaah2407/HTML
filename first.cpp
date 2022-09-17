52294734f9d07f6e03ed8b7b0f1a333b14266a7e
Varshaah Shashidhar <79756939+varshaah2407@users.noreply.github.com>
2022-09-17 16:23:00 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() 
+{
+    // Sum of two numbers
+    int a,b;
+    cout<<"Enter first number: ";
+    cin>>a;
+    cout<<"Enter second number: ";
+    cin>>b;
+
+    cout<<"The sum is: "<<a+b;
+
+    return 0;  
+}
